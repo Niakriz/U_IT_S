d04de0ca7ad0353d5c57663ec58c84b4248949d5
Niakriz <niakriz.nia@gmail.com>
2015-11-11 01:58:11 +0200

    Task1

---
+++ HomeTasks/HomeTask5/src/main/java/Task1.java
@@ -0,0 +1,35 @@
+/**
+ * Created by user on 11.11.2015.
+ */
+
+public class Task1 {
+
+
+
+    public static void main (String[] args){
+
+        int[] mass = {7,4,6,3,5,2,1};
+
+        for (int i = 0; i < mass.length - 1; i++){
+            for (int j = 0; j < mass.length - i -1; j++){
+
+                if (mass[j] > mass[j + 1]){
+                  /** int temp = mass[j];
+                    mass[j] = mass[j + 1];
+                    mass[j + 1] = temp; */
+                    int a = mass[j];
+                    int b = mass[j + 1];
+                    int c;
+                    c = a;
+                    a = b;
+                    b = c;
+
+                    System.out.println(mass[i]);
+                }
+
+
+            }
+        }
+
+    }
+}
---
+++ HomeTasks/HomeTask5/src/main/java/Task3.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 11.11.2015.
+ */
+public class Task3 {
+    public static void main (String[] args){
+        
+    }
+}
