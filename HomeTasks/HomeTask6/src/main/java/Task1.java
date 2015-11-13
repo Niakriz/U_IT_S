d005729f8ec271698a7e21d826e9b580f92e25b8
Niakriz <niakriz.nia@gmail.com>
2015-11-14 01:41:21 +0200

    Matrix

---
+++ HomeTasks/HomeTask6/src/main/java/Task1.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Nia on 14.11.2015.
+ */
+public class Task1 {
+
+    static public void main(String[] args){
+
+       int[][] mass = new int[6][6];
+
+
+
+        for (int i = 0; i<mass.length;i++){
+            for (int j = 0; j<mass.length;j++){
+
+                System.out.print(i + " ");
+
+                mass[i][j] = i;
+
+            }System.out.println();
+        }
+        System.out.println();
+        for (int i = 0 ; i < mass.length; i++ ){
+            for (int j = 0; j< mass[i].length; j++){
+                System.out.print(mass[j][i]+" ");
+            }System.out.println();
+        }
+
+
+    }
+
+    }
