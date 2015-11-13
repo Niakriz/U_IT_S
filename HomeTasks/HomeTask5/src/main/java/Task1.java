bb3f433506865df5b3a50ec16a7ec7b1270207f5
Niakriz <niakriz.nia@gmail.com>
2015-11-13 19:19:31 +0200

    Stas edition :)

--- HomeTasks/HomeTask5/src/main/java/Task1.java
+++ HomeTasks/HomeTask5/src/main/java/Task1.java
@@ -24,12 +24,29 @@ public class Task1 {
                     a = b;
                     b = c;
 
-                    System.out.println(mass[i]);
+
                 }
 
 
             }
+
         }
+
+        for (int i = 0; i < mass.length - 1; i++){
+            for(int j = 0; j < mass.length - i - 1; j++){
+
+                if(mass[j] > mass[j + 1]){
+                    int temp = mass[j];
+                    mass[j] = mass[j + 1];
+                    mass[j + 1] = temp;
+
+                }
+            }
+
+        }
+        for (int i = 0; i < mass.length; i++){
+            System.out.print(mass[i]+" " );
+        } System.out.println();
 
     }
 }
